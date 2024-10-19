b36b5d3800eec40c907baab79c1a4c6a1cd6cbea
sadeghjfr <sadeghjfr22@gmail.com>
2024-10-19 11:03:24 +0330

    Add composer autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/housesale-php-mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7aebf1a79bc40cbc8774ee7b3a56fd23e51f542d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/housesale-php-mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7aebf1a79bc40cbc8774ee7b3a56fd23e51f542d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
