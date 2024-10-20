fb0f9ea46ba606f6abe09c33183bbf011ee601ea
sadeghjfr <sadeghjfr22@gmail.com>
2024-10-20 19:37:05 +0330

    Add base model

---
+++ system/Database/ORM/Model.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace System\Database\ORM;
+
+use System\Database\Traits\HasAttributes;
+use System\Database\Traits\HasCRUD;
+use System\Database\Traits\HasMethodCaller;
+use System\Database\Traits\HasQueryBuilder;
+use System\Database\Traits\HasRelation;
+use System\Database\Traits\HasSoftDelete;
+
+abstract class Model{
+
+    use HasAttributes, HasCRUD, HasMethodCaller, HasQueryBuilder, HasRelation, HasSoftDelete;
+
+    protected $table;
+    protected $fillable = [];
+    protected $hidden = [];
+    protected $casts = [];
+    protected $primaryKey = 'id';
+    protected $createdAt = 'created_at';
+    protected $updatedAt = 'updated_at';
+    protected $deletedAt = 'deleted_at';
+    protected $collection = [];
+
+}
