85ac5237d8d9d015d4a5b4e1b4f1131af52b0d05
sadeghjfr <sadeghjfr22@gmail.com>
2024-10-24 11:07:09 +0330

    Update bootstrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,64 +1,46 @@
 <?php
-// dd helper function
 
-use System\Database\Traits\HasCRUD;
+session_start();
 
-function dd($var){
-    echo "<pre>";
-    print_r($var);
-    exit;
+if(isset($_SESSION['temporary_flash'])) unset($_SESSION['temporary_flash']);
+if(isset($_SESSION['temporary_errorFlash'])) unset($_SESSION['temporary_errorFlash']);
+if(isset($_SESSION['old'])) unset($_SESSION['temporary_old']);
+
+if(isset($_SESSION['flash']))
+{
+    $_SESSION['temporary_flash'] = $_SESSION['flash'];
+    unset($_SESSION['flash']);
 }
+
+if(isset($_SESSION['errorFlash']))
+{
+    $_SESSION['temporary_errorFlash'] = $_SESSION['errorFlash'];
+    unset($_SESSION['errorFlash']);
+}
+
+if(isset($_SESSION['old']))
+{
+    $_SESSION['temporary_old'] = $_SESSION['old'];
+    unset($_SESSION['old']);
+}
+
+$params = [];
+$params = !isset($_GET) ? $params : array_merge($params, $_GET);
+$params = !isset($_POST) ? $params : array_merge($params, $_POST);
+$_SESSION['old'] = $params;
+unset($params);
+
+require_once ("../system/helpers/helper.php");
+
 
 require_once ("../config/app.php");
 require_once ("../config/database.php");
 
 
+global $routes;
+$routes = ['get' => [], 'post' => [], 'put' => [], 'delete' => []];
 require_once ("../routes/web.php");
 require_once ("../routes/api.php");
-
-
-
-/*$categories = \App\Category::all();
-foreach($categories as $category){
-    echo $category->id . '--' . $category->name . '--';
-}*/
-
-/* $categories = \App\Category::paginate(2);
- foreach($categories as $category){
-     echo $category->id . '--' . $category->name . '--';
- }*/
-
-// $post = \App\Post::find(1);
-// dd($post);
-
-// $categories = \App\Category::where('id', '>', 1)->get();
-// foreach($categories as $category){
-//     echo $category->id . '--' . $category->name . '--';
-// }
-
-
-// $posts = \App\Category::find(1)->posts()->get();
-// foreach($posts as $post){
-//     echo $post->id . '--' . $post->title . '--';
-// }
-
-
-// $post = \App\Post::find(1);
-// $category = $post->category();
-// dd($category);
-
-// $users = \App\Role::find(1)->users()->get();
-// foreach($users as $user){
-//     echo $user->id . '--' . $user->username . '--';
-// }
-
-
- $roles = \App\User::find(1)->roles()->get();
- foreach($roles as $role){
-     echo $role->id . '--' . $role->name . '--';
- }
-
-
 
 
 $routing = new \System\Router\Routing();
