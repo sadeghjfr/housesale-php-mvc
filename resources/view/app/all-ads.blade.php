013febc369dbccea385b8a887f510c0a549ad5f4
sadeghjfr <sadeghjfr22@gmail.com>
2024-10-28 19:28:51 +0330

    Add home ads paginate

--- resources/view/app/all-ads.blade.php
+++ resources/view/app/all-ads.blade.php
@@ -1,81 +1,68 @@
 @extends('app.layouts.app')
 
 @section('head-tag')
-<title>آکهی ها</title>
+    <title>آکهی ها</title>
 @endsection
 
 @section('content')
 
-<div class="hero-wrap" style="background-image: url('images/bg_1.jpg');">
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center">
-            <div class="col-md-9 ftco-animate text-center">
-                <p class="breadcrumbs"><span class="mr-2"><a href="index.html">خانه</a></span> <span>آگهی ها</span></p>
-                <h1 class="mb-3 bread">آگهی ها</h1>
+    <div class="hero-wrap" style="background-image: url('images/bg_1.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row no-gutters slider-text align-items-center justify-content-center">
+                <div class="col-md-9 ftco-animate text-center">
+                    <p class="breadcrumbs"><span class="mr-2"><a href="index.html">خانه</a></span> <span>آگهی ها</span>
+                    </p>
+                    <h1 class="mb-3 bread">آگهی ها</h1>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
 
-<section class="ftco-section bg-light">
-    <div class="container">
-        <div class="row">
-            <?php foreach($ads as $advertise) { ?>
-            <div class="col-md-4 ftco-animate">
-                <div class="properties">
-                    <a href="<?= route('home.ads', [$advertise->id]) ?>" class="img img-2 d-flex justify-content-center align-items-center" style="background-image: url('<?= asset($advertise->image) ?>');">
-                        <div class="icon d-flex justify-content-center align-items-center">
-                            <span class="icon-search2"></span>
-                        </div>
-                    </a>
-                    <div class="text p-3">
-                        <span class="status <?= $advertise->sellStatus() == 'خرید' ? 'sale' : 'rent' ?>"><?= $advertise->sellStatus() ?></span>
-                        <div class="d-flex">
-                            <div class="one">
-                                <h3><a href="<?= route('home.ads', [$advertise->id]) ?>"><?= $advertise->title ?></a></h3>
-                                <p><?= $advertise->type() ?></p>
+    <section class="ftco-section bg-light">
+        <div class="container">
+            <div class="row">
+                <?php foreach (paginate($ads, 2) as $advertise) { ?>
+                <div class="col-md-4 ftco-animate">
+                    <div class="properties">
+                        <a href="<?= route('home.ads', [$advertise->id]) ?>"
+                           class="img img-2 d-flex justify-content-center align-items-center"
+                           style="background-image: url('<?= asset($advertise->image) ?>');">
+                            <div class="icon d-flex justify-content-center align-items-center">
+                                <span class="icon-search2"></span>
                             </div>
-                            <div class="two">
-                                <span class="price"><?= $advertise->amount ?></span>
+                        </a>
+                        <div class="text p-3">
+                            <span class="status <?= $advertise->sellStatus() == 'خرید' ? 'sale' : 'rent' ?>"><?= $advertise->sellStatus() ?></span>
+                            <div class="d-flex">
+                                <div class="one">
+                                    <h3>
+                                        <a href="<?= route('home.ads', [$advertise->id]) ?>"><?= $advertise->title ?></a>
+                                    </h3>
+                                    <p><?= $advertise->type() ?></p>
+                                </div>
+                                <div class="two">
+                                    <span class="price"><?= $advertise->amount ?></span>
+                                </div>
                             </div>
+                            <p><?= html_entity_decode(substr($advertise->description, 0, 100)) ?></p>
+                            <hr>
+                            <p class="bottom-area d-flex">
+                                <span><i class="flaticon-selection"></i> ۲۹۰ متر</span>
+                                <span class="ml-auto"><i class="flaticon-bathtub"></i> ۳</span>
+                                <span><i class="flaticon-bed"></i> ۳</span>
+                            </p>
                         </div>
-                        <p><?= html_entity_decode(substr($advertise->description, 0, 100 )) ?></p>
-                        <hr>
-                        <p class="bottom-area d-flex">
-                            <span><i class="flaticon-selection"></i> ۲۹۰ متر</span>
-                            <span class="ml-auto"><i class="flaticon-bathtub"></i> ۳</span>
-                            <span><i class="flaticon-bed"></i> ۳</span>
-                        </p>
                     </div>
                 </div>
+                <?php } ?>
+
+
             </div>
-            <?php } ?>
-      
-     
-  
+            <?= paginateView($ads, 2) ?>
         </div>
-        <div class="row mt-5">
-            <div class="col text-center">
-                <div class="block-27">
-                    <ul>
-                        <li><a href="#">&lt;</a></li>
-                        <li class="active"><span>1</span></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
-                        <li><a href="#">&gt;</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-
-
+    </section>
 
 @endsection
